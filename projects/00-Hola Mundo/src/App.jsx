9b73bb8731b36d2c899bb9c8b31b6e79694b0486
tobiasR2004 <tobi.26romani@gmail.com>
2025-06-17 18:15:53 -0300

    "Commit 1: xCard"

--- projects/00-Hola Mundo/src/App.jsx
+++ projects/00-Hola Mundo/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App(){
+    return(
+    <article className='x-followCard'>
+        <header className="x-followCard-header">
+            <img className="x-followCard-img" alt="icono" src="https://i.pinimg.com/222x/57/70/f0/5770f01a32c3c53e90ecda61483ccb08.jpg"></img>
+            <div className="x-followCard-header-div">   
+                <strong>  Tobias Romani </strong>
+                <span className="x-followCard-header-div-cuenta"> @TobiasR26 </span>
+            </div>
+        </header>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+
+        <aside>
+            <button className="x-followCard-button">
+                Seguir
+            </button>
+        </aside>
+    </article>
+    )
 }
-
-export default App
